87cc1cd713bddd74b983e3291f8449b9926b404a
gulcin <gulcin_375@hotmail.com>
2025-08-30 23:11:25 +0300

    feat(router): setup React Router with Home, Signup, Login, and Onboarding pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import OnboardingPage from "./pages/OnboardingPage";
+
 const App = () => {
   return (
     <div className="h-screen" data-theme="night">
-      App
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+      </Routes>
     </div>
   );
 };
