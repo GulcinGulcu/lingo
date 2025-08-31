f1af20e96a41c2b3db45934216c06c2af5742dcc
gulcin <gulcin_375@hotmail.com>
2025-08-31 18:23:14 +0300

    feat: add sign up page UI and handle signing up functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
@@ -7,13 +7,39 @@ import { useQuery } from "@tanstack/react-query";
 import { axiosInstance } from "./lib/axios.js";
 
 const App = () => {
+  const {
+    data: authData,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry: false,
+  });
+
+  const authUser = authData?.user;
   return (
-    <div className="h-screen" data-theme="night">
+    <div className="h-screen" data-theme="dracula">
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/onboarding"
+          element={authUser ? <OnboardingPage /> : <Navigate to={"/"} />}
+        />
       </Routes>
     </div>
   );
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,189 @@
+import { useState } from "react";
+import Logo from "../../icons/Logo";
+import { Link } from "react-router";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { signup } from "../lib/api.js";
+
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  const [signupData, setSignupData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  const queryClient = useQueryClient();
+
+  const {
+    mutate: signupMutation,
+    isPending,
+    error,
+  } = useMutation({
+    mutationFn: signup,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+  });
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    signupMutation(signupData);
+  };
+
+  return (
+    <div className="h-screen flex justify-center items-center p-4 sm:p-6 lg:p-8">
+      <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-4xl mx-auto bg-base-100 rounded-xl shadow-xl overflow-hidden">
+        {/* Left side */}
+        <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+          <div className="mb-4 flex items-center justify-start gap-2">
+            <Logo />
+            <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
+              Lingo
+            </span>
+          </div>
+          {error && (
+            <div className="alert alert-error mb-4">
+              <span>{error.response.data.message}</span>
+            </div>
+          )}
+          <div className="w-full">
+            <form onSubmit={handleSignup}>
+              <div className="space-y-4">
+                <div>
+                  <h2 className="text-xl font-semibold">Create an account</h2>
+                  <p className="text-sm opacity-70">
+                    Join Lingo today and start your language learning adventure!
+                  </p>
+                </div>
+                <div className="space-y-3">
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Full Name</span>
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="John Doe"
+                      className="input input-bordered w-full"
+                      value={signupData.fullName}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...signupData,
+                          fullName: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                  </div>
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Email</span>
+                    </label>
+                    <input
+                      type="email"
+                      placeholder="example@gmail.com"
+                      className="input input-bordered w-full"
+                      value={signupData.email}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...signupData,
+                          email: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                  </div>
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Password</span>
+                    </label>
+                    <input
+                      type="password"
+                      placeholder="*******"
+                      className="input input-bordered w-full"
+                      value={signupData.password}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...signupData,
+                          password: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                    <p className="text-xs opacity-60 mt-1">
+                      Password must be at least 6 characters.
+                    </p>
+                  </div>
+                  <div className="form-control">
+                    <label className="label cursor-pointer justify-start gap-2">
+                      <input
+                        type="checkbox"
+                        className="checkbox checkbox-sm required:"
+                        required
+                      />
+                      <span className="text-xs leading-tight">
+                        I agree{" "}
+                        <span className="text-primary hover:underline">
+                          terms and conditions
+                        </span>{" "}
+                        and{" "}
+                        <span className="text-primary hover:underline">
+                          privacy policy
+                        </span>
+                      </span>
+                    </label>
+                  </div>
+                  <button
+                    className={`btn w-full ${
+                      isPending ? "btn-neutral" : "btn-primary"
+                    }`}
+                    type="submit"
+                  >
+                    {isPending ? (
+                      <>
+                        <span className="loading loading-spinner loading-xs"></span>
+                        Signing Up...
+                      </>
+                    ) : (
+                      "Create Account"
+                    )}
+                  </button>
+                  <div className="text-center mt-5">
+                    <p className="text-sm ml-3">
+                      Already have an account?{" "}
+                      <Link
+                        to={"/login"}
+                        className="text-primary hover:underline"
+                      >
+                        Sign In
+                      </Link>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        {/* Right side */}
+        <div className="hidden lg:flex w-full lg:w-1/2 bg-primary/5 items-center justify-center">
+          <div className="max-w-md p-8">
+            <div className="relative aspect-square max-w-sm mx-auto">
+              <img
+                src="/call-amico.png"
+                alt="application illustration"
+                className="w-full h-full"
+              />
+            </div>
+            <div className="text-center mt-5">
+              <h2 className="text-lg font-semibold">
+                Connect with language partners
+              </h2>
+              <p className="text-sm opacity-70">
+                Practice conversations, make friends and improve your language
+                skills
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
